1886ecb845657817de6196bfd950dc17bec2c300
Jonathan Garache <jongarache@gmail.com>
2020-06-24 19:21:44 -0700

    Add files via upload

---
+++ ball.cpp
@@ -0,0 +1,21 @@
+#include "ball.h"
+
+float ball::getPosX(){
+	return this->posX;
+}
+
+float ball::getPosY(){
+	return this->posY;
+}
+
+float ball::getVelX(){
+	return this->velX;
+}
+
+float ball::getVelY(){
+	return this->velY;
+}
+
+ball::ball(){
+
+}
---
+++ ball.h
@@ -0,0 +1,20 @@
+#pragma once
+class ball
+{
+public:
+	float getPosX();
+	float getPosY();
+	float getVelX();
+	float getVelY();
+	ball();
+
+private:
+	float posX;
+	float posY;
+	float velX;
+	float velY;
+
+
+
+};
+
